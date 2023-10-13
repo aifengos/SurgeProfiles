c2a89fa596ab5e682520655874ba034cb2faade8
aifengos <128759049+aifengos@users.noreply.github.com>
2023-10-13 08:19:05 +0800

    Update amoydx_dns.js
    Add primaryRouter Match

--- Surge/Rewrite/amoydx_dns.js
+++ Surge/Rewrite/amoydx_dns.js
@@ -1,13 +1,15 @@
 var ssids = [
     'BIT-WIFI-5G','amoydx'
 ];
+var routes = ["10.6.18.1"];
 var ssid = $network.wifi.ssid;
-//console.log(said);
+var route = $network.v4.primaryRouter
+//console.log($network);
 if (ssid === null) {
     ssid = ''  // 有线网
 };
 
-if (ssids.includes(ssid)) {
+if (ssids.includes(ssid) || (routes.includes(route))) {
     $done({server:'192.168.2.156'})
 } else {
     $done({})
