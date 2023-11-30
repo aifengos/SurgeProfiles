6102d9b36587e33174dc56fa93a514953fb1ca1e
aifengos <128759049+aifengos@users.noreply.github.com>
2023-11-30 11:41:37 +0800

    Update amoydx_dns.js

--- Surge/Rewrite/amoydx_dns.js
+++ Surge/Rewrite/amoydx_dns.js
@@ -1,15 +1,16 @@
-var ssids = [
-    'BIT-WIFI-5G','amoydx'
-];
-var routes = ["10.6.18.1"];
-var ssid = $network.wifi.ssid;
+//var ssids = [
+//    'BIT-WIFI-5G','amoydx'
+//];
+var routes = ["10.6.18.1", "10.6.18.254"];
+//var ssid = $network.wifi.ssid;
 var route = $network.v4.primaryRouter
 //console.log($network);
-if (ssid === null) {
-    ssid = ''  // 有线网
-};
+//if (ssid === null) {
+//    ssid = ''  // 有线网
+//};
 
-if (ssids.includes(ssid) || (routes.includes(route))) {
+// if (ssids.includes(ssid) || (routes.includes(route))) {
+if (routes.includes(route)) {
     $done({server:'192.168.2.156'})
 } else {
     $done({})
